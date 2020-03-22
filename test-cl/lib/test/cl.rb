e25bcff30a66314719a71cdf4812b8b8d822ba2b
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2020-03-22 00:31:04 +0000

    Automatically backed up by Learn

--- test-cl/lib/test/cl.rb
+++
@@ -1,8 +0,0 @@
-require "test/cl/version"
-
-module Test
-  module Cl
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- test-cl/lib/test/cl/version.rb
+++
@@ -1,5 +0,0 @@
-module Test
-  module Cl
-    VERSION = "0.1.0"
-  end
-end
